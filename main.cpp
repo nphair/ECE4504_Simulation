6fb482e5ed8b8f4d0cd25b3e34fa3a19321062f7
Lucas Kirchhoff <lucaskirchhoff1@gmail.com>
2017-12-04 00:50:48 -0500

    Added inputs for main

--- main.cpp
+++ main.cpp
@@ -3,8 +3,20 @@
 #include "server.hpp"
 #include "request.hpp"
 #include <iostream>
+#include <cstdlib>
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Welcome to thunderdome." << std::endl;
+    if (argc < 4)
+    {
+        std::cout << "Not all proper variables included:\nExpected Input: SERVER_NUM SERVER_PER_RACK STORAGE_PER_SERVER";
+        exit(0);
+    }
+    int serverNum = atoi(argv[1]);
+    int seversPerRack = atoi(argv[2]);
+    int serverStorage = atoi(argv[3]);
+
+    int loadBalanceWork = 10;
+    int serverWork = 10;
+    LoadBalancer * masterLoadBalancer = new LoadBalancer(NULL, loadBalanceWork);
 }
